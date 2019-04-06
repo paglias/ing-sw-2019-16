440e0c81f90c3649165bc5ea7b233448c1350dbc
brunosciarrone <bruno.sciarrone@mail.polimi.it>
2019-04-06 12:05:38 +0200

    Create generic classes skulls and square with attributes

---
+++ src/main/java/models/Square.java
@@ -0,0 +1,16 @@
+package models;
+
+import java.util.ArrayList;
+
+public class Square {
+    protected Color color;
+    private int position;
+    private Boolean IsSpawnPoint;
+    protected ArrayList<Square> canView;
+    protected ArrayList<Square> canAccessDirectly;
+
+
+
+
+
+}
