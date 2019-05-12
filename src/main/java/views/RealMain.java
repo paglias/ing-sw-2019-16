383a36137b3ce2308cf43dc47414fc665c153cb0
andreipricope <48279828+andreipricope@users.noreply.github.com>
2019-05-12 13:57:25 +0200

    view finally works

---
+++ src/main/java/views/RealMain.java
@@ -0,0 +1,10 @@
+package views;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class RealMain { //the real class
+    public static void main(String[] args) {
+        Game.main(args);
+    }
+}
