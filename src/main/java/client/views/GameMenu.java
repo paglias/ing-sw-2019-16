93cd56121881450e753884711ef59023aa6d794b
andreipricope <48279828+andreipricope@users.noreply.github.com>
2019-05-15 21:42:53 +0200

    add methods on buttons

--- src/main/java/client/views/GameMenu.java
+++ src/main/java/client/views/GameMenu.java
@@ -1,4 +1,17 @@
 package client.views;
 
 public class GameMenu {
+
+    public void chooseMap1(){
+    }
+    public void chooseMap2(){
+    }
+    public void chooseMap3(){
+    }
+    public void chooseMap4(){
+    }
+    public void setNickname(){
+    }
+    public void nextWindow(){
+    }
 }
