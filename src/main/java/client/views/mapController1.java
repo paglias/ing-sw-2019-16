dbe7bfd314d9ac3cfc9e742df9207c12f34ba2a5
andreipricope <48279828+andreipricope@users.noreply.github.com>
2019-05-21 16:53:11 +0200

    Add username on players GUI

--- src/main/java/client/views/mapController1.java
+++ src/main/java/client/views/mapController1.java
@@ -1,8 +1,11 @@
 package client.views;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.SplitPane;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import java.net.URL;
@@ -17,12 +20,30 @@ public class mapController1 implements Initializable {
     AnchorPane imageAnchorPane;
     @FXML
     Button quitButton;
+    @FXML
+    SplitPane horizontalSplit;
+    @FXML
+    SplitPane verticalSplit;
+    @FXML
+    SplitPane horizontalSplit2;
 
     //Sets map size correctly
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         image1.fitWidthProperty().bind(imageAnchorPane.widthProperty());
         image1.fitHeightProperty().bind(imageAnchorPane.heightProperty());
+
+        //Locks the split dividers
+
+        SplitPane.Divider divider = horizontalSplit.getDividers().get(0);
+        divider.positionProperty().addListener(new ChangeListener<Number>()
+        {
+            @Override
+            public void changed(ObservableValue<? extends Number> observable, Number oldvalue, Number newvalue )
+            {
+                divider.setPosition(0.5);
+            }
+        });
     }
 
     //Show possible settings? //TODO
