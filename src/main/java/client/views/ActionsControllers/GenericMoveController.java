5016a6f4209f4c82fca8aa8f9c9ad650ec0053cb
andreipricope <48279828+andreipricope@users.noreply.github.com>
2019-06-23 15:58:17 +0200

    Add positions on move windows

--- src/main/java/client/views/ActionsControllers/GenericMoveController.java
+++ src/main/java/client/views/ActionsControllers/GenericMoveController.java
@@ -1,27 +1,58 @@
 package client.views.ActionsControllers;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 //Window that lets the user select a new position and confirm it.
 // Confirm sends a message to server.
 
-public class GenericMoveController {
+public class GenericMoveController implements Initializable {
 
-    @FXML private Button confirm;
+    @FXML public Button confirm;
 
-    @FXML private ChoiceBox<String> newPosition;
+    @FXML public ChoiceBox<String> newPosition;
 
     @FXML void confirmMove(ActionEvent event) {
-
+        String positionSelected = newPosition.getSelectionModel().getSelectedItem();
+        //TODO SEND POSITION TO SERVER
     }
 
+    @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //TODO ADD POSITIONS
+        ArrayList<String> positions= new ArrayList();
+        positions.add("0");
+        positions.add("1");
+        positions.add("2");
+        positions.add("2");
+        positions.add("4");
+        positions.add("5");
+        positions.add("6");
+        positions.add("7");
+        positions.add("8");
+        positions.add("9");
+        positions.add("10");
+        positions.add("11");
+        ObservableList<String> availableChoices = FXCollections.observableArrayList(
+                positions.get(0),
+                positions.get(1),
+                positions.get(2),
+                positions.get(3),
+                positions.get(4),
+                positions.get(5),
+                positions.get(6),
+                positions.get(7),
+                positions.get(8),
+                positions.get(9),
+                positions.get(10),
+                positions.get(11));
+        newPosition.setItems(availableChoices);
     }
 }
