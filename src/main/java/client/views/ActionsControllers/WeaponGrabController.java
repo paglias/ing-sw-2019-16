53dcd0d9b88570d9bb14f3359ffa86abf3b9acdc
Matteo Pagliazzi <matteopagliazzi@gmail.com>
2019-06-27 10:30:41 +0200

    WeaponGrabController names

--- src/main/java/client/views/ActionsControllers/WeaponGrabController.java
+++ src/main/java/client/views/ActionsControllers/WeaponGrabController.java
@@ -8,8 +8,14 @@ import javafx.scene.control.Button;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.ToggleGroup;
 import messages.ActionMessage;
+import messages.GameStateMessage;
 import messages.client_data.ClientInput;
+import messages.client_data.SquareData;
+import messages.client_data.WeaponData;
+import messages.client_data.WeaponsSlotData;
+
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class WeaponGrabController implements Initializable {
@@ -17,14 +23,14 @@ public class WeaponGrabController implements Initializable {
     public String weaponChosen;
 
     @FXML private Button confirm;
-    @FXML private RadioButton weaponOne;
     @FXML private ToggleGroup weaponSelected;
+
+    @FXML private RadioButton weaponOne;
     @FXML private RadioButton weaponTwo;
     @FXML private RadioButton weaponThree;
 
     //Sets the selected weapon and sends it to server
     @FXML void confirmGrab(ActionEvent event) {
-
         if (weaponOne.isSelected()){
             weaponChosen =  weaponOne.getText();
         }
@@ -47,7 +53,28 @@ public class WeaponGrabController implements Initializable {
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        //TODO SETTEXT TO weaponOne,Two,Three FROM GAMESTATEMESSAGE. THE WEAPONS ARE THE CURRENT WEAPONSSLOT WEAPONS
-        //TODO get weapons from current weaponsslot?
+        GameStateMessage gameStateMessage = Game.controller.getLastGameStateMessage();
+
+        int playerPosition = gameStateMessage.playerYouData.position;
+        SquareData square = gameStateMessage.gameBoardData.squares.get(playerPosition);
+        WeaponsSlotData slot = square.weaponsSlot;
+        ArrayList<WeaponData> weapons = slot.weapons;
+
+        int wIndex = 0;
+
+        for (WeaponData weapon : weapons) {
+            switch (wIndex) {
+                case 0:
+                    weaponOne.setText(weapon.name);
+                    break;
+                case 1:
+                    weaponTwo.setText(weapon.name);
+                    break;
+                case 2:
+                    weaponThree.setText(weapon.name);
+                    break;
+            }
+            wIndex++;
+        }
     }
 }
